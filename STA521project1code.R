edd8d019789f84811b0523cce7b8fbf1a4887d14
TianyuTerry <jnwutianyu@gmail.com>
2022-09-18 19:11:09 -0400

    Part 2 (a), (b) and (c) with some bugs

--- STA521project1code.R
+++ STA521project1code.R
@@ -1,11 +1,17 @@
-## 2. Data Cleaning
+### This is the R code demo for project 1 of STA 521, Fall 2022 ###
 
+### 2(a) Check histogram of each variable. 
+log_data <- read_csv("~/sta 521 proj1/sonoma-data-log.csv")
+net_data <- read_csv("~/sta 521 proj1/sonoma-data-net.csv")
 
+# Variable voltage
 
-## 3. Data Exploration 
+log_data %>%
+  ggplot(aes(x = voltage)) +
+  geom_histogram()
 
+net_data %>%
+  ggplot(aes(x = voltage)) +
+  geom_histogram()
 
-## 4. Interesting Findings
-
-
-## 5. Graph Critique in the paper
+# Variable humidity
